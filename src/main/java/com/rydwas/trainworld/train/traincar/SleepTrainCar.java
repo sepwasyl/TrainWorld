f372b273c1651614b56f10a30f3a72424ec449f7
sepwasyl <120589737+sepwasyl@users.noreply.github.com>
2023-04-10 21:00:51 +0200

    Added extensions to locomotives and train cars
    a few types of locomotive and train cars have been added and arguments to them.

---
+++ src/main/java/com/rydwas/trainworld/train/traincar/SleepTrainCar.java
@@ -0,0 +1,11 @@
+package com.rydwas.trainworld.train.traincar;
+
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+
+public class SleepTrainCar extends TrainCar {
+
+    @Getter
+    @NotNull
+    private int numberOfBeds;
+}
